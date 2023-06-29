fb0886ba9619ddc520c395d80264a8dca4f49719
Mark De May <mark1ad@users.noreply.github.com>
2023-06-28 19:46:06 -0600

    Parse -floors argument

--- src/Elevator.java
+++ src/Elevator.java
@@ -1,3 +1,7 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * Provide code that simulates an elevator. You are free to use any language.
  * Upload the completed project to GitHub for discussion during interview.
@@ -8,15 +12,16 @@
  *                   Program Constants: Single floor travel time: 10
  *
  * Assumptions:
- * 1) Assumes that floor numbers can be negative an that ground floor is 0.
+ * 1) Assumes that floor numbers can be negative and that the first floor is 0.
  */
 public class Elevator {
 
+    public static final String FLOORS_OPTION = "-floors";
     public static final String START_OPTION = "-start";
 
     public static void main(String[] args) {
         int startingFloor = getStartingFloor(args);
-        System.out.println(startingFloor);
+        List<Integer> floors = getFloorsToVisit(args);
     }
 
     /**
@@ -52,11 +57,48 @@ public class Elevator {
     }
 
     /**
+     * Get the list of floors to visit.
+     * @param args  arguments passed into program
+     * @return      List of floors to visit
+     */
+    private static List<Integer> getFloorsToVisit(String[] args) {
+        List<String> floorsStrList;
+        List<Integer> floorsList = null;
+
+        for (int i = 0; i < args.length; i++) {
+            // Look for -floors and convert the next arg to a list of integers for the floors
+            // that the elevator will visit.
+            if (args[i].equals(FLOORS_OPTION) && i < args.length - 1) {
+                floorsStrList = new ArrayList<String>(Arrays.asList( args[i+1].split(",")));
+                floorsList = new ArrayList<Integer>();
+                for (String floor: floorsStrList) {
+                    try {
+                        floorsList.add(Integer.parseInt(floor));
+                    }
+                    catch (NumberFormatException e) {
+                        System.out.println("ERROR: -floors option values must all be numbers");
+                        printUsage();
+                    }
+                }
+            }
+        }
+
+        // Check if floors option was found.
+        if (floorsList == null) {
+            System.out.println("ERROR: -floors option not found.");
+            printUsage();
+        }
+
+        return floorsList;
+    }
+
+    /**
      * Print out the program's usage message and exit with a value of 1.
      */
     private static void printUsage() {
-        System.out.println("usage: elevator -start n");
+        System.out.println("usage: elevator -start n -floors x,y,...");
         System.out.println("\t-start n\tspecify the floor that the elevator starts on.");
+        System.out.println("\t-floors x,y,...\tThe floors the elevator will stop at. Value is a comma separated list of numbers. Can be a single number.");
         System.exit(1);
 
     }
